07c0d5b13b83eca46bd8b41237b2af17423f7a15
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-04-03 17:39:30 -0300

    att

---
+++ aula11/ex011/js/index.js
@@ -0,0 +1,11 @@
+function verificar(){
+    var nacionalidade = document.querySelector('#nacionality')
+    var response = document.querySelector('#res')
+    response.innerHTML = `<p>O país que você mora é ${nacionalidade.value}, certo?</p>`
+    if (String(nacionalidade.value).toLowerCase() == 'brasil') {
+        response.innerHTML += `<p>Como vai você, querido conterrâneo!?</p>`
+    } else {
+        response.innerHTML += `<p>Se estiver correto, você é estrangeiro!!!</p>`
+    }
+}
+    
