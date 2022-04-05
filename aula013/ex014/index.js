5e14b80416e3261ac91fc050566242209c63a329
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-04-04 23:10:17 -0300

    commit on main

--- aula013/ex014/index.js
+++ aula013/ex014/index.js
@@ -1,15 +1,15 @@
 var hora = new Date().getHours();
-window.document.querySelector('p#hours_container').innerHTML = `Agora são ${hora} horas.`
+document.querySelector('p#hours_container').innerHTML = `Agora são ${hora} horas.`
 if (12 > hora && hora >= 6) {
     //manhã
-    window.document.querySelector('body').style.backgroundColor = 'rgb(255, 196, 0)'
-    window.document.querySelector('#today_image').src = 'https://www.10wallpaper.com/wallpaper/1366x768/1212/Kayaks_morning-Natural_landscape_Photography_Wallpaper_1366x768.jpg'
+    document.querySelector('body').style.backgroundColor = 'rgb(255, 196, 0)'
+    document.querySelector('#today_image').src = 'https://www.10wallpaper.com/wallpaper/1366x768/1212/Kayaks_morning-Natural_landscape_Photography_Wallpaper_1366x768.jpg'
 } else if (18 > hora && hora >= 12){
     //tarde
-    window.document.querySelector('body').style.backgroundColor = 'orangered'
-    window.document.querySelector('#today_image').src = 'https://i.pinimg.com/originals/26/b5/a6/26b5a6044e434c49781b4a8a3cfe85c5.jpg'
+    document.querySelector('body').style.backgroundColor = 'orangered'
+    document.querySelector('#today_image').src = 'https://i.pinimg.com/originals/26/b5/a6/26b5a6044e434c49781b4a8a3cfe85c5.jpg'
 } else {
     //noite
-    window.document.querySelector('body').style.backgroundColor = 'rgb(42, 40, 40)'
-    window.document.querySelector('#today_image').src = 'https://i.pinimg.com/originals/bc/88/2c/bc882c4a00eb417c655026731ae35255.jpg'
+    document.querySelector('body').style.backgroundColor = 'rgb(42, 40, 40)'
+    document.querySelector('#today_image').src = 'https://i.pinimg.com/originals/bc/88/2c/bc882c4a00eb417c655026731ae35255.jpg'
 }
--- aula013/teste.js
+++
@@ -1,8 +0,0 @@
-var x = 19 
-if (x >= 18) {
-    console.log('Aproveite seu pornô!')
-} else if (x >= 16){
-    console.log('Deixa pelo menos crescer os pentelho')
-} else {
-    console.log('Tu não deveria nem estar na net')
-}
