9c290b93f39b17b8293094a999f92ef7a6789de0
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-04-07 18:16:32 -0300

    commit on main

--- aula019/ex001/index.js
+++ aula019/ex001/index.js
@@ -1,10 +1,61 @@
-let numero = document.querySelector('#adicionado').value;
-var add_local = document.querySelector('#table');
+var num = document.querySelector('input#adicionado')
+var lista = document.querySelector('select#table')
+var res = document.querySelector('div#result')
+var valores = []
 
-function added(){
-    if (numero >= 1 && numero <= 100){
-        add_local.innerHTML = `Valor ${numero} adicionado.`;
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        window.alert(`O número é ${numero} esse`);
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finished() {
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(var pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma dos valores é igual a ${soma}.</p>`
+        res.innerHTML += `<p>A média é igual a ${media}.</p>`
     }
 }
